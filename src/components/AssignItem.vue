7f55a86b24b3eb7bd42c9166c7eada1169cae525
SMFaysalHaque <smfaysalhaque007@gmail.com>
2024-05-13 17:23:25 +0600

    feat: update close button for item and conditionally active and deactive add and close button.

--- src/components/AssignItem.vue
+++ src/components/AssignItem.vue
@@ -25,7 +25,7 @@
             </div>
             <div
                 v-for="(singleProduct, i) in filterProduct"
-                :key="singleProduct"
+                :key="i"
                 class=""
             >
                 <div class="flex justify-between items-center py-1">
@@ -35,8 +35,19 @@
                             brandProductModal(singleProduct.categoryName, i)
                         "
                         class="border border-green-700 bg-green-700 hover:bg-green-500 hover:border-green-500 rounded-sm text-white px-2 me-1"
+                        :class="{'hidden' : selectedIndex == i, 'pointer-events-none opacity-50' : selectedIndex !== -1}"
+                        :disabled="selectedIndex !== -1"
                     >
                         add
+                    </button>
+                    <button
+                        class="border border-red-600 bg-red-600 hover:bg-red-500 w-5 h-5 rounded-full ps-[1px]"
+                        :class="selectedIndex != i ? 'hidden' : 'block'"
+                        @click="closeItem(i)"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none">
+                        <path d="M4.70703 3.29297L3.29297 4.70703L10.5859 12L3.29297 19.293L4.70703 20.707L12 13.4141L19.293 20.707L20.707 19.293L13.4141 12L20.707 4.70703L19.293 3.29297L12 10.5859L4.70703 3.29297Z" fill="#FFFF"/>
+                        </svg>
                     </button>
                 </div>
                 <div v-if="singleProduct.isActive">
@@ -68,6 +79,7 @@ export default {
             filterProduct: [],
             uniqueProductCategory: {},
             selectedCategoryName: "",
+            selectedIndex: -1
         };
     },
     mounted() {
@@ -94,11 +106,18 @@ export default {
         },
         brandProductModal(value, i) {
             console.log(value);
+            console.log(i);
+            this.selectedIndex = i
+            console.log('Selected Index:::', this.selectedIndex);
             // value.isActive = true
             this.filterProduct[i]["isActive"] = true;
             // this.selectedCategoryName = value.category
             this.selectedCategoryName = value;
         },
+        closeItem (i) {
+            this.filterProduct[i]["isActive"] = false
+            this.selectedIndex = -1
+        }
     },
 };
 </script>
